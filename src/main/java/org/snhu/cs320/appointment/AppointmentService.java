122b39a9b667ef0b25c85d60b5b3938e07ebff3a
David Faulkner <d@davo.co>
2024-06-03 01:30:38 -0500

    Finish AppointmentService class

--- src/main/java/org/snhu/cs320/appointment/AppointmentService.java
+++ src/main/java/org/snhu/cs320/appointment/AppointmentService.java
@@ -1,5 +1,23 @@
 package org.snhu.cs320.appointment;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class AppointmentService {
+	
+	static Map<String, Appointment> APPOINTMENT_DATABASE = 
+			new ConcurrentHashMap<String, Appointment>();
+	
+	private AppointmentService() {}
+	
+	public static boolean add(Appointment appointment) {
+		if (APPOINTMENT_DATABASE.containsKey(appointment.getId())) return false;
+		APPOINTMENT_DATABASE.putIfAbsent(appointment.getId(), appointment);
+		return true;
+	}
+	
+	public static boolean delete(String id) {
+		return APPOINTMENT_DATABASE.remove(id) != null;
+	}
 
 }
